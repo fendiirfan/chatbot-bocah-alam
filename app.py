e90aca07e8945e88a582ddbbf562bbe3e500e7e6
Fendi Irfan Amorokhman <56022249+fendiirfan@users.noreply.github.com>
2023-01-17 21:25:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,50 +18,49 @@ st.image(image,width=100,)
 st.text("")
 
 
-while 1==1:
-    # inputan user
-    user_input = st.text_input('what can I do for you?')
+# inputan user
+user_input = st.text_input('what can I do for you?')
 
-    # tombols
-    button = st.button('Send')
+# tombols
+button = st.button('Send')
 
 
-    stt_button = Button(label="Speak", width=100)
+stt_button = Button(label="Speak", width=100)
 
-    stt_button.js_on_event("button_click", CustomJS(code="""
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
             }
         }
-        recognition.start();
-        """))
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
 
-    result = streamlit_bokeh_events(
-        stt_button,
-        events="GET_TEXT",
-        key="listen",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0)
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-    if result: # Speak
-        if "GET_TEXT" in result:
-            st.write("Your command : \n",result.get("GET_TEXT"))
-            with st.spinner('Typing...'):
-              st.markdown("ChatBot Response : \n")
-              st.write(chatBot(result.get("GET_TEXT")))
-    elif button==True: # submit text input
+if result: # Speak
+    if "GET_TEXT" in result:
+        st.write("Your command : \n",result.get("GET_TEXT"))
         with st.spinner('Typing...'):
           st.markdown("ChatBot Response : \n")
-          st.write(chatBot(user_input))  
+          st.write(chatBot(result.get("GET_TEXT")))
+elif button==True: # submit text input
+    with st.spinner('Typing...'):
+      st.markdown("ChatBot Response : \n")
+      st.write(chatBot(user_input))  
